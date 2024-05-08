db674b1ee738bf65778faaf81e1650310801dd80
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2024-05-08 17:20:25 -0300

    algoritmo huffman

--- huffman/main.c
+++ huffman/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include "tree.h"
-// #include "list.h"
+// // #include "tree.h"
+// // #include "list.h"
 #include "types.h"
 
 int main()
